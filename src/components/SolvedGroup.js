2c9271c09bb951f4048526520b3b0c44fee3acf7
Dave Fetterman <fettermania@amperity.com>
2025-03-26 13:44:17 -0700

    Add SolvedGroup component

--- src/components/SolvedGroup.js
+++ src/components/SolvedGroup.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const SolvedGroup = ({ group, colorClass }) => {
+const SolvedGroup = ({ group, index }) => {
+  const colorClasses = ['solved-yellow', 'solved-green', 'solved-blue', 'solved-purple'];
+  const colorClass = colorClasses[index % colorClasses.length];
+  
   return (
     <div className={`solved-group ${colorClass}`}>
-      <div className="solved-group-title">
-        {group.name}
-      </div>
+      <div className="solved-group-title">{group.category}</div>
       <div className="solved-group-emojis">
-        {group.emojis.map((emoji, index) => (
-          <span key={index}>{emoji}</span>
+        {group.emojis.map((emoji, i) => (
+          <span key={i}>{emoji}</span>
         ))}
       </div>
     </div>
