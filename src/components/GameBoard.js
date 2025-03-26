d4b74f20cae937434c99ea5ceb6d396a2d2dfde0
Dave Fetterman <fettermania@amperity.com>
2025-03-26 13:44:04 -0700

    Add GameBoard component

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import EmojiTile from './EmojiTile';
 import SolvedGroup from './SolvedGroup';
-import GameOverModal from './GameOverModal';
 
 const GameBoard = ({ 
   gameData, 
@@ -13,38 +12,49 @@ const GameBoard = ({
   isGameOver,
   isGameWon
 }) => {
-  // Get all unsolved emojis
-  const getUnsolvedEmojis = () => {
-    // Get all emojis that are not part of a solved group
+  // Get all emoji tiles to display
+  const getAvailableEmojis = () => {
+    // Get all emojis that haven't been solved yet
     const solvedEmojis = solvedGroups.flatMap(group => group.emojis);
-    
-    // Create an array of all emojis from all groups in the game data
     const allEmojis = gameData.groups.flatMap(group => group.emojis);
     
-    // Filter out the solved emojis
     return allEmojis.filter(emoji => !solvedEmojis.includes(emoji));
   };
 
-  const unsolvedEmojis = getUnsolvedEmojis();
+  // Check if tile is in a solved group and return the group color class
+  const getSolvedGroupClass = (emoji) => {
+    for (let i = 0; i < solvedGroups.length; i++) {
+      if (solvedGroups[i].emojis.includes(emoji)) {
+        const colorClasses = ['solved-yellow', 'solved-green', 'solved-blue', 'solved-purple'];
+        return colorClasses[i % colorClasses.length];
+      }
+    }
+    return '';
+  };
 
-  // Color classes for solved groups
-  const colorClasses = [
-    'solved-yellow',
-    'solved-green', 
-    'solved-blue', 
-    'solved-purple'
-  ];
+  const availableEmojis = getAvailableEmojis();
 
   return (
     <div className="game-board">
-      {/* Unsolved emojis grid */}
+      {/* Display solved groups */}
+      <div className="solved-groups">
+        {solvedGroups.map((group, index) => (
+          <SolvedGroup 
+            key={index}
+            group={group}
+            index={index}
+          />
+        ))}
+      </div>
+
+      {/* Display emoji grid */}
       <div className="emojis-grid">
-        {unsolvedEmojis.map((emoji, index) => (
-          <EmojiTile 
+        {availableEmojis.map((emoji, index) => (
+          <EmojiTile
             key={index}
             emoji={emoji}
             isSelected={selectedEmojis.includes(emoji)}
-            isSolved={false}
+            solvedGroupClass={getSolvedGroupClass(emoji)}
             onClick={() => onEmojiSelect(emoji)}
           />
         ))}
@@ -53,38 +63,39 @@ const GameBoard = ({
       {/* Game controls */}
       <div className="game-controls">
         <button 
-          className="game-button submit-button"
+          className="btn btn-shuffle" 
+          onClick={onShuffle}
+          disabled={isGameOver || availableEmojis.length === 0}
+        >
+          Shuffle
+        </button>
+        <button 
+          className="btn btn-submit" 
           onClick={onSubmit}
           disabled={selectedEmojis.length !== 4 || isGameOver}
         >
           Submit
         </button>
-        <button 
-          className="game-button shuffle-button"
-          onClick={onShuffle}
-          disabled={isGameOver}
-        >
-          Shuffle
-        </button>
       </div>
 
-      {/* Display solved groups */}
-      <div className="solved-groups">
-        {solvedGroups.map((group, index) => (
-          <SolvedGroup 
-            key={index}
-            group={group}
-            colorClass={colorClasses[index]} 
-          />
-        ))}
-      </div>
-
-      {/* Game over modal */}
+      {/* Game over message */}
       {isGameOver && (
-        <GameOverModal 
-          isWinner={isGameWon}
-          solvedGroupsCount={solvedGroups.length}
-        />
+        <div className="game-over-overlay">
+          <div className="game-over-message">
+            <h2>{isGameWon ? 'Congratulations!' : 'Game Over!'}</h2>
+            <p>
+              {isGameWon 
+                ? 'You successfully completed all connections!' 
+                : 'You ran out of attempts. Better luck next time!'}
+            </p>
+            {!isGameWon && solvedGroups.length > 0 && (
+              <p>You solved {solvedGroups.length} out of 4 connections.</p>
+            )}
+            <button className="btn" onClick={() => window.location.reload()}>
+              Play Again
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
